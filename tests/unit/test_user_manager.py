0b12f11fa78963d48ef4ff398f272e3a4d2c6af5
Grant Ramsay <seapagan@gmail.com>
2023-06-02 08:41:13 +0100

    catch sqlite duplicate email error and test it
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/unit/test_user_manager.py
+++ tests/unit/test_user_manager.py
@@ -1,6 +1,5 @@
 """Test the UserManager class."""
-from sqlite3 import IntegrityError
-
+from fastapi import HTTPException
 import pytest
 
 from managers.user import UserManager
@@ -16,12 +15,10 @@ class TestUserManager:
         "last_name": "User",
     }
 
-    @pytest.mark.skip(reason="fails under SQLite")
     @pytest.mark.asyncio()
     async def test_create_duplicate_user(self, get_db):
         """Test creating a duplicate user."""
         await UserManager.register(self.test_user.copy(), get_db)
-        await UserManager.register(self.test_user, get_db)
 
-        with pytest.raises(IntegrityError):
+        with pytest.raises(HTTPException, match="already exists"):
             await UserManager.register(self.test_user, get_db)
