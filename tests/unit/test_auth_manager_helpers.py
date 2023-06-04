e2d5c5bbed5f07cb2fb774a5ce6bea9f40630364
Grant Ramsay <seapagan@gmail.com>
2023-06-04 10:02:50 +0100

    refactor auth helper tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/unit/test_auth_manager_helpers.py
+++ tests/unit/test_auth_manager_helpers.py
@@ -6,75 +6,61 @@ from managers.auth import can_edit_user, is_admin, is_banned
 from models.enums import RoleType
 
 
-class TestAuthManager:
+@pytest.fixture()
+def mock_req(mocker):
+    """Fixture to return a mocked Request object."""
+    request_mock_path = "managers.auth.Request"
+    return mocker.patch(request_mock_path)
+
+
+class TestAuthManagerHelpers:
     """Test the AuthManager class."""
 
-    request_mock_path = "managers.auth.Request"
-
-    # --------------- test the dependency_injector helpers work -------------- #
-    def test_is_admin_allow_admin(
-        self,
-        mocker,
-    ):
+    # ----------------- test the dependency_injector helpers ----------------- #
+    def test_is_admin_allow_admin(self, mock_req):
         """Test the is_admin method returns no exception for admin users."""
-        mock_request = mocker.patch(self.request_mock_path)
-        mock_request.state.user = {"role": RoleType.admin}
+        mock_req.state.user = {"role": RoleType.admin}
 
-        assert is_admin(mock_request) is None
+        assert is_admin(mock_req) is None
 
-    def test_is_admin_block_non_admin(
-        self,
-        mocker,
-    ):
+    def test_is_admin_block_non_admin(self, mock_req):
         """Test the is_admin method returns an exception for non-admin users."""
-        mock_request = mocker.patch(self.request_mock_path)
-        mock_request.state.user = {"role": RoleType.user}
+        mock_req.state.user = {"role": RoleType.user}
 
         with pytest.raises(HTTPException, match="Forbidden"):
-            is_admin(mock_request)
+            is_admin(mock_req)
 
-    def test_is_banned_blocks_banned_user(self, mocker):
+    def test_is_banned_blocks_banned_user(self, mock_req):
         """Test the is_banned method blocks banned users."""
-        mock_request = mocker.patch("managers.auth.Request")
-        mock_request.state.user = {"banned": True}
+        mock_req.state.user = {"banned": True}
 
         with pytest.raises(HTTPException, match="Banned!"):
-            is_banned(mock_request)
+            is_banned(mock_req)
 
-    def test_is_banned_ignores_valid_user(self, mocker):
+    def test_is_banned_ignores_valid_user(self, mock_req):
         """Test the is_banned method allows non-banned users through."""
-        mock_request = mocker.patch("managers.auth.Request")
-        mock_request.state.user = {"banned": False}
+        mock_req.state.user = {"banned": False}
 
-        assert is_banned(mock_request) is None
+        assert is_banned(mock_req) is None
 
-    def test_can_edit_user_allow_admin(
-        self,
-        mocker,
-    ):
+    def test_can_edit_user_allow_admin(self, mock_req):
         """Test the can_edit_user method returns no exception for admin."""
-        mock_request = mocker.patch(self.request_mock_path)
-        mock_request.state.user = {"role": RoleType.admin, "id": 2}
-        mock_request.path_params = {"user_id": 1}
+        mock_req.state.user = {"role": RoleType.admin, "id": 2}
+        mock_req.path_params = {"user_id": 1}
 
-        assert can_edit_user(mock_request) is None
+        assert can_edit_user(mock_req) is None
 
-    def test_can_edit_user_allow_owner(
-        self,
-        mocker,
-    ):
+    def test_can_edit_user_allow_owner(self, mock_req):
         """Test the can_edit_user method returns no exception for the owner."""
-        mock_request = mocker.patch(self.request_mock_path)
-        mock_request.state.user = {"role": RoleType.user, "id": 1}
-        mock_request.path_params = {"user_id": 1}
+        mock_req.state.user = {"role": RoleType.user, "id": 1}
+        mock_req.path_params = {"user_id": 1}
 
-        assert can_edit_user(mock_request) is None
+        assert can_edit_user(mock_req) is None
 
-    def test_can_edit_user_block_non_admin(self, mocker):
+    def test_can_edit_user_block_non_admin(self, mock_req):
         """Test the can_edit_user method returns an exception for non-admin."""
-        mock_request = mocker.patch(self.request_mock_path)
-        mock_request.state.user = {"role": RoleType.user, "id": 2}
-        mock_request.path_params = {"user_id": 1}
+        mock_req.state.user = {"role": RoleType.user, "id": 2}
+        mock_req.path_params = {"user_id": 1}
 
         with pytest.raises(HTTPException, match="Forbidden"):
-            can_edit_user(mock_request)
+            can_edit_user(mock_req)
