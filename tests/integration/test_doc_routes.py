0b085f2f35c9d239df2f13337ddf2ec6e9b71ef3
Grant Ramsay <seapagan@gmail.com>
2023-06-07 21:10:14 +0100

    basic integration test for swagger docs
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ tests/integration/test_doc_routes.py
@@ -0,0 +1,27 @@
+"""Test the Swagger UI routes."""
+import pytest
+
+from config.settings import get_settings
+
+
+@pytest.mark.integration()
+class TestDocRoutes:
+    """Test the Swagger documentation."""
+
+    def test_swagger_ui_html(self, test_app):
+        """Test the Swagger UI docs are accessible.
+
+        We just do very basic testing here to ensure that the docs are being
+        displayed. We don't test the content of the docs themselves, except to
+        ensure that the title is correct and the HTML is valid
+        """
+        response = test_app.get("/docs")
+
+        assert response.status_code == 200
+        assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+        assert response.text.strip().startswith("<!DOCTYPE html>")
+        assert (
+            f"<title>{get_settings().api_title} | Documentation</title>"
+            in response.text
+        )
