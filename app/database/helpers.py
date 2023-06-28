27168f98d49d12b2bb064a8c9f3e9f39a0b8d738
Grant Ramsay <seapagan@gmail.com>
2023-06-28 13:32:53 +0100

    rename user helper functions
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/database/helpers.py
+++ app/database/helpers.py
@@ -6,19 +6,19 @@ from sqlalchemy import select
 from app.models.user import User
 
 
-async def _get_all_users(session) -> List[Dict]:
+async def get_all_users_(session) -> List[Dict]:
     """Return all Users in the database."""
     result = await session.execute(select(User))
     return result.scalars().all()
 
 
-async def _get_user_by_email(email, session):
+async def get_user_by_email_(email, session):
     """Return a specific user by their email address."""
     result = await session.execute(select(User).where(User.email == email))
     return result.scalars().first()
 
 
-async def _get_user_by_id(user_id: int, session):
+async def get_user_by_id_(user_id: int, session):
     """Return a specific user by their email address."""
     result = await session.execute(select(User).where(User.id == user_id))
     return result.scalars().first()
