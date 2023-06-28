a84188a77d2bc4e3fa3f892e9aff76b0d5fba3ce
Grant Ramsay <seapagan@gmail.com>
2023-06-28 10:39:11 +0100

    migrate database setup
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/database/db.py
+++ app/database/db.py
@@ -1,21 +1,23 @@
 """Setup the Database and support functions.."""
-import databases
-import sqlalchemy
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
 
 from app.config.settings import get_settings
 
 DATABASE_URL = (
-    f"postgresql://{get_settings().db_user}:{get_settings().db_password}@"
+    "postgresql+asyncpg://"
+    f"{get_settings().db_user}:{get_settings().db_password}@"
     f"{get_settings().db_address}:{get_settings().db_port}/"
     f"{get_settings().db_name}"
 )
 
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
+engine = create_async_engine(DATABASE_URL, echo=False)
+Base = declarative_base()
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_database():
     """Return the database connection as a Generator."""
-    await database.connect()
-    yield database
-    await database.disconnect()
+    async with async_session() as session:
+        async with session.begin():
+            yield session
