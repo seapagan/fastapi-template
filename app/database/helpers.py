6c86b8fc352e284d85c2efef7dec66515800c9fd
Grant Ramsay <seapagan@gmail.com>
2023-06-28 13:31:22 +0100

    add some User helpers
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ app/database/helpers.py
@@ -0,0 +1,24 @@
+"""Define some database helper functions."""
+from typing import Dict, List
+
+from sqlalchemy import select
+
+from app.models.user import User
+
+
+async def _get_all_users(session) -> List[Dict]:
+    """Return all Users in the database."""
+    result = await session.execute(select(User))
+    return result.scalars().all()
+
+
+async def _get_user_by_email(email, session):
+    """Return a specific user by their email address."""
+    result = await session.execute(select(User).where(User.email == email))
+    return result.scalars().first()
+
+
+async def _get_user_by_id(user_id: int, session):
+    """Return a specific user by their email address."""
+    result = await session.execute(select(User).where(User.id == user_id))
+    return result.scalars().first()
