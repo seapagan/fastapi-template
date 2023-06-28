af158f398d208b239f7093da8fbac57aea5e9bb3
Grant Ramsay <seapagan@gmail.com>
2023-06-28 14:51:01 +0100

    typing to database.helpers
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/database/helpers.py
+++ app/database/helpers.py
@@ -1,24 +1,24 @@
 """Define some database helper functions."""
-from typing import Dict, List
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.user import User
 
 
-async def get_all_users_(session) -> List[Dict]:
+async def get_all_users_(session: AsyncSession):
     """Return all Users in the database."""
     result = await session.execute(select(User))
     return result.scalars().all()
 
 
-async def get_user_by_email_(email, session):
+async def get_user_by_email_(email, session: AsyncSession) -> User | None:
     """Return a specific user by their email address."""
     result = await session.execute(select(User).where(User.email == email))
     return result.scalars().first()
 
 
-async def get_user_by_id_(user_id: int, session):
+async def get_user_by_id_(user_id: int, session: AsyncSession) -> User | None:
     """Return a specific user by their email address."""
     result = await session.execute(select(User).where(User.id == user_id))
     return result.scalars().first()
