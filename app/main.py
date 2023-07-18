a55e6bc89e72071a7fe9fe0033e55b6ba1a04b87
Grant Ramsay <seapagan@gmail.com>
2023-07-18 09:20:50 +0100

    remove inused import
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.staticfiles import StaticFiles
 from rich import print  # pylint: disable=W0622
 
