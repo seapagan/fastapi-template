96685b1b5a0a23b778a5ca8bfea1d79540ffc880
Grant Ramsay <seapagan@gmail.com>
2023-07-19 20:52:22 +0100

    re-enable some unit tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ tests/helpers.py
@@ -0,0 +1,17 @@
+"""Some helper functions for testing."""
+import jwt
+
+from app.config.settings import get_settings
+
+
+def get_token(sub: int, exp: float, typ: str) -> str:
+    """Return a JWT token."""
+    return jwt.encode(
+        {
+            "sub": sub,
+            "exp": exp,
+            "typ": typ,
+        },
+        get_settings().secret_key,
+        algorithm="HS256",
+    )
