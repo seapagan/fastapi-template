1677cd7206eab358a4c841a3ee567f2a6bfccfa3
Grant Ramsay <seapagan@gmail.com>
2023-09-22 17:10:57 +0100

    change env var to detect GitHub actions
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ from app.database.db import Base, get_database
 from app.main import app
 from app.managers.email import EmailManager
 
-if os.getenv("GITHUB_ACTIONS"):
+if os.getenv("CI"):
     DATABASE_URL = (
         "postgresql+asyncpg://postgres:postgres"
         "@localhost:5432/fastapi-template-test"
