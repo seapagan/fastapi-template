d19073f0852b4d67ba76930a03e20eb1351000c8
Grant Ramsay <seapagan@gmail.com>
2023-11-13 08:18:46 +0000

    fix crash d/t fastapi bug when return 204 status
    crashes if the 204 function is annotated with return 'None' and
    future annotations is imported
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/database/helpers.py
+++ app/database/helpers.py
@@ -1,7 +1,6 @@
 """Define some database helper functions."""
 from __future__ import annotations
 
-from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 from sqlalchemy import select
@@ -9,6 +8,8 @@ from sqlalchemy import select
 from app.models.user import User
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
