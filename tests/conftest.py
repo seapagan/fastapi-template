f91354ea51c040977f4f85576b4148efccb933bb
Grant Ramsay <seapagan@gmail.com>
2023-12-12 12:49:31 +0000

    lock pytest-asyncio to 0.21.1
    release 0.23.x breaks custom event loops which are needed for this project atm
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,7 +54,7 @@ def event_loop(request) -> Generator[asyncio.AbstractEventLoop, Any, Any]:
 
     This is required for pytest-asyncio to work with FastAPI.
     """
-    loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
