c94e243ba28385977a8c25b7913309c4f5e2d2dc
Grant Ramsay <seapagan@gmail.com>
2023-12-18 16:52:43 +0000

    get app.database.db module to 100% coverage
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ tests/unit/test_database.py
@@ -0,0 +1,19 @@
+"""Test functions in the app.database module."""
+import pytest
+
+from app.database import db
+
+
+class TestDatabase:
+    """Class to test the database module."""
+
+    @pytest.mark.asyncio()
+    async def test_get_database(self) -> None:
+        """Test we get an async database session back."""
+        database = db.get_database()
+        assert database is not None
+        assert isinstance(database, db.AsyncGenerator)
+
+        session = await database.__anext__()
+        assert session is not None
+        assert isinstance(session, db.AsyncSession)
