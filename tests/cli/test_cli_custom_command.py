dff105beac89e3553bb036a56fb1088c4937d64d
Grant Ramsay <seapagan@gmail.com>
2023-12-18 15:42:40 +0000

    refactor custom.py a little
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/cli/test_cli_custom_command.py
+++ tests/cli/test_cli_custom_command.py
@@ -12,6 +12,7 @@ from app.commands.custom import (
     choose_license,
     choose_version,
     get_case_insensitive_dict,
+    get_data,
     get_licenses,
     init,
 )
@@ -23,6 +24,21 @@ class TestCLI:
 
     metadata_path = Path("/home/test/metadata.json")
     mock_get_config_path = "app.commands.custom.get_config_path"
+    home_dir = "/home/test"
+
+    test_data = {
+        "title": "Test Title",
+        "desc": "Test Description",
+        "version": "0.5.0",
+        "repo": "https://myrepo.com",
+        "license": {
+            "name": "MIT",
+            "url": "https://opensource.org/licenses/MIT",
+        },
+        "author": "test_user",
+        "email": "test_user@test.com",
+        "website": "https://mysite.com",
+    }
 
     def test_no_command_should_give_help(self, runner: CliRunner) -> None:
         """Test that running with no command should give help."""
@@ -36,12 +52,12 @@ class TestCLI:
         assert all(command in result.output for command in command_list)
 
     def test_init_function(self, fs, mocker) -> None:
-        """Test that running 'init' should create a deafult metadata."""
+        """Test that running 'init' should create a default metadata."""
         mock_get_config_path = mocker.patch(
             self.mock_get_config_path,
             return_value=self.metadata_path,
         )
-        fs.create_dir("/home/test")
+        fs.create_dir(self.home_dir)
 
         assert not self.metadata_path.exists()
 
@@ -56,7 +72,7 @@ class TestCLI:
             self.mock_get_config_path,
             return_value=self.metadata_path,
         )
-        fs.create_dir("/home/test")
+        fs.create_dir(self.home_dir)
         fs.create_file(
             self.metadata_path,
             contents='{"title": "Test Title"}',
@@ -79,7 +95,7 @@ class TestCLI:
             side_effect=OSError("File Error"),
         )
 
-        fs.create_dir("/home/test")
+        fs.create_dir(self.home_dir)
 
         with pytest.raises(typer.Exit):
             init()
@@ -90,6 +106,26 @@ class TestCLI:
         assert "File Error" in output
 
     # ----------------------- test individual functions ---------------------- #
+    def test_get_input_from_user(self, mocker) -> None:
+        """Test that running the ."""
+        test_input = (
+            "Test Title\nTest Description\n0.5.0\nhttps://myrepo.com\nMIT\ntest_user\n"
+            "test_user@test.com\nhttps://mysite.com\n\n"
+        )
+        mocker.patch.object(sys, "stdin", io.StringIO(test_input))
+
+        input_values = get_data()
+
+        assert isinstance(input_values, dict)
+        assert input_values["title"] == "Test Title"
+        assert input_values["desc"] == "Test Description"
+        assert input_values["version"] == "0.5.0"
+        assert input_values["repo"] == "https://myrepo.com"
+        assert input_values["license"]["name"] == "MIT"
+        assert input_values["author"] == "test_user"
+        assert input_values["email"] == "test_user@test.com"
+        assert input_values["website"] == "https://mysite.com"
+
     def test_get_licenses_function(self) -> None:
         """Test that running 'get_licenses' should return a list of licenses."""
         licenses = get_licenses()
@@ -138,7 +174,7 @@ class TestCLI:
 
     def test_choose_license(self, mocker, capsys) -> None:
         """Test that the choose license function works."""
-        mocker.patch.object(sys, "stdin", io.StringIO("mit\n"))
+        mock_stdin = mocker.patch.object(sys, "stdin", io.StringIO("mit\n"))
 
         license_string = ", ".join(
             [license_name["name"] for license_name in LICENCES]
@@ -152,3 +188,5 @@ class TestCLI:
 
         assert isinstance(license_choice, dict)
         assert license_choice["name"] == "MIT"
+
+        assert mock_stdin.read
