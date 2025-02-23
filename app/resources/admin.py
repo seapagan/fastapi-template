b666bcaaf39f646d490efd8e69aed0e7fb00b16f
Grant Ramsay <seapagan@gmail.com>
2025-02-23 13:06:57 +0000

    start adding auth to the admin pages
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- app/resources/admin.py
+++ app/resources/admin.py
@@ -1,15 +1,60 @@
 """Set up the admin interface."""
 
-from typing import Any, ClassVar, Union
+from __future__ import annotations
 
-from fastapi import FastAPI, Request
+from typing import TYPE_CHECKING, Any, ClassVar, Union
+
 from sqladmin import Admin, ModelView
-from sqlalchemy.orm.attributes import InstrumentedAttribute
+from sqladmin.authentication import AuthenticationBackend
 
 from app.database.db import async_session
-from app.database.helpers import hash_password
+from app.database.helpers import (
+    get_user_by_email_,
+    hash_password,
+    verify_password,
+)
 from app.models.api_key import ApiKey
+from app.models.enums import RoleType
 from app.models.user import User
+
+if TYPE_CHECKING:
+    from fastapi import FastAPI, Request
+    from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+
+class AdminAuth(AuthenticationBackend):
+    """Setup the authentication backend for the admin interface."""
+
+    async def login(self, request: Request) -> bool:
+        """Login the user."""
+        db = async_session()
+        form = await request.form()
+        email = form.get("username")
+        password = form.get("password")
+        if not email or not password:
+            return False
+        user = await get_user_by_email_(email, db)
+        await db.close()
+        if (
+            not user
+            or not verify_password(password, user.password)
+            or user.role != RoleType.admin
+            or user.banned
+        ):
+            return False
+
+        request.session.update({"user_id": user.id})
+        return True
+
+    async def logout(self, request: Request) -> bool:
+        """Logout the user."""
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> bool:
+        """Authenticate the user."""
+        user_id = request.session.get("user_id")
+        return user_id is not None
 
 
 class KeysAdmin(ModelView, model=ApiKey):
@@ -109,6 +154,12 @@ class UserAdmin(ModelView, model=User):
 
 def register_admin(app: FastAPI) -> None:
     """Register the admin views."""
-    admin = Admin(app, session_maker=async_session)
+    authentication_backend = AdminAuth(secret_key="secret")
+
+    admin = Admin(
+        app,
+        session_maker=async_session,
+        authentication_backend=authentication_backend,
+    )
     admin.add_view(UserAdmin)
     admin.add_view(KeysAdmin)
